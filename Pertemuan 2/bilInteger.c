326ff640772d4a2df07027708d767b67d9e21fb8
tiaraputriw <126688819+tiaraputriw@users.noreply.github.com>
2023-03-16 22:04:53 +0700

    Update bilInteger.c

--- Pertemuan 2/bilInteger.c
+++ Pertemuan 2/bilInteger.c
@@ -1,6 +1,6 @@
 /*Nama File : bilInteger.c*/
 /*Deskripsi : Mengecek dan menampilkan bilangan bulat positif atau nol atau bilabgan bulat negatif*/
-/*Pembuat   : Tiara Putri Wibowo*/
+/*Pembuat   : Tiara Putri Wibowo - 24060122120026*/
 /*Tanggal   : Rabu, 01 Maret 2023, 10.11*/
 
 
